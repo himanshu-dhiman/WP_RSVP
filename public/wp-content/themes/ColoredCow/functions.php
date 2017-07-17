6b0c6ac82451faa99b33c0c0530d727fa7cc9d6a
himanshu-dhiman <himanshu@coloredcow.in>
2017-07-17 14:06:24 +0530

    Resolved Issues

--- public/wp-content/themes/ColoredCow/functions.php
+++ public/wp-content/themes/ColoredCow/functions.php
@@ -2,17 +2,16 @@
 
 if ( ! function_exists( 'cc_scripts' ) ) {
     function cc_scripts() {
-        wp_enqueue_script('cc-jquery-script','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
-        wp_enqueue_script('cc-bootstrap-script','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js');
+        wp_enqueue_script('cc-bootstrap4-script', get_template_directory_uri().'/dist/lib/js/bootstrap4.min.js', array('jquery'), '1.0.0', true);
+        wp_enqueue_script('cc-bootstrap-tether','https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js');
         wp_enqueue_script('cc-fontawesome-icons','https://use.fontawesome.com/ffc2c94a85.js');
-        wp_localize_script( 'main', 'PARAMS', array('ajaxurl' => admin_url('admin-ajax.php')) );
    }
     add_action('wp_enqueue_scripts','cc_scripts');
 }
 
 if ( ! function_exists( 'cc_styles' ) ) {
-    function cc_styles() {  
-        wp_enqueue_style('cc-bootstrap-css','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css');
+    function cc_styles() { 
+        wp_enqueue_style('cc-bootstrap4-style', get_template_directory_uri().'/dist/lib/css/bootstrap4.min.css');
         wp_enqueue_style('cc-font-oswald','https://fonts.googleapis.com/css?family=Oswald');
         wp_enqueue_style('cc-font-marcellus-sc','https://fonts.googleapis.com/css?family=Marcellus SC');
         wp_enqueue_style('cc-font-roboto','https://fonts.googleapis.com/css?family=Roboto');
