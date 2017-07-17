29eaf977c6c6a435f64f6ec825a877649416d1ca
himanshu-dhiman <himanshu@coloredcow.in>
2017-07-17 19:15:19 +0530

    #7-Added a Common footer, used fonts.css

--- public/wp-content/themes/ColoredCow/footer.php
+++ public/wp-content/themes/ColoredCow/footer.php
@@ -1,4 +1,7 @@
 	<?php wp_footer(); ?>
 </body>
-
+	<div class="footer">
+		<hr>
+		<p>Copyright &copy; ColoredCow || 2017 </p>
+	</div>
 </html>
--- public/wp-content/themes/ColoredCow/landing.php
+++ public/wp-content/themes/ColoredCow/landing.php
@@ -14,10 +14,10 @@
 					<br>
 						<p class="lead" >ColoredCow celebrates every first Saturday of the month with family and friends. This custom has been started to take a little time off from work and enjoy some moments in life. we believe in sharing moments and learning with each other. Come and join us over music, food, drinks and some moments full of laughter and joy.</p>
 					<hr>
-				</div>
 			</div>
 		</div>
-	</body>
+	</div>
+</body>
 <?php 
     get_footer(); 
 ?>
