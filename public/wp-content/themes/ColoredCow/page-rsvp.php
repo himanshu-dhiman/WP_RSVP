a30320a3fd12e9f589b3c8ec034f67f38264f133
himanshu-dhiman <himanshu@coloredcow.in>
2017-07-20 11:50:35 +0530

    Changes in Style.css

--- public/wp-content/themes/ColoredCow/page-rsvp.php
+++ public/wp-content/themes/ColoredCow/page-rsvp.php
@@ -9,16 +9,15 @@
 <body>
 	<div class="container">
 	<br>
-	<div class="card-title text-center thank-you-text">You've Confirmed your presence, Thank You! </div>
-    <hr>
+        <div class="card-title text-center thank-you-text">Looking forward to see you at the event.</div>
+	<hr>
         <div class="guest-details text-center">
             <div><i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;&nbsp;Himanshu Dhiman
             </div>
             <div><i class="fa fa-envelope-open-o" aria-hidden="true"></i>&nbsp;&nbsp;himanshu@coloredcow.in</div>
             <div><i class="fa fa-mobile" aria-hidden="true"></i>&nbsp;&nbsp;+91-9808014227</div>
             <hr>
-            <div class="card-title text-center thank-you-text">Looking forward to see you at the event.</div>
-            <a href="<?php echo home_url(); ?>" class="btn btn-success btn-lg ">Back to Home</a>
+            <a href="<?php echo home_url(); ?>" class="btn btn-success btn-lg ">Confirm Your Presence</a>
         </div>
     </div>
 </body>
